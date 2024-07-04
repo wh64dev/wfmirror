3f75bf52ee688661d83b778848f9652de38e5ba6
WH64 <me@wh64.net>
2024-07-04 14:33:58 +0900

    fix: entries data null problem fixed

--- components/Render.jsx
+++ components/Render.jsx
@@ -4,29 +4,36 @@ import styles from "./render.module.scss";
 
 /**
  * @author WH64
- * @param { string } url
- * @param { any } data
  * @returns { JSX.Element }
  */
 export function Render({ url, data }) {
+	let render = <></>;
+	if (data.data !== null) {
+		render = (
+			<>
+				{data.data.map((file, index) => {
+					if (file.type === "dir") {
+						return (
+							<a key={index} href={`/${file.url}`}>
+								{file.name}/
+							</a>
+						);
+					}
+
+					return (
+						<a key={index} href={`${url}/path/${file.url}`}>
+							{file.name}
+						</a>
+					);
+				})}
+			</>
+		);
+	}
+
 	return (
 		<>
 			<h2>{data.dir !== "" ? data.dir : "/"}</h2>
-			{data.data.map((file, index) => {
-				if (file.type === "dir") {
-					return (
-						<a key={index} href={`/${file.url}`}>
-							{file.name}/
-						</a>
-					);
-				}
-
-				return (
-					<a key={index} href={`${url}/path/${file.url}`}>
-						{file.name}
-					</a>
-				);
-			})}
+			{render}
 		</>
 	);
 }
