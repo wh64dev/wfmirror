db9982c8275c1508bfafe60a3a279ba7c0b5996e
WH64 <kimpiao020@gmail.com>
2024-07-05 23:57:21 +0900

    feat: nodeinfo routes created

--- routes/mod.go
+++ routes/mod.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"fmt"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/wh64dev/wfcloud/config"
 	"github.com/wh64dev/wfcloud/middleware"
+	"github.com/wh64dev/wfcloud/util"
 )
 
 func New(app *gin.Engine) {
@@ -25,4 +27,29 @@ func New(app *gin.Engine) {
 		})
 	})
 	app.GET("/path/*dirname", dirWorker.List)
+	app.GET("/nodeinfo", func(ctx *gin.Context) {
+		start := time.Now()
+		cnf := config.Get()
+		var stats syscall.Statfs_t
+		err := syscall.Statfs(cnf.Global.DataDir, &stats)
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"ok":    0,
+				"errno": err.Error(),
+			})
+			return
+		}
+
+		total := stats.Blocks * uint64(stats.Bsize)
+		used := total - stats.Bfree*uint64(stats.Bsize)
+
+		ctx.JSON(200, gin.H{
+			"ok": 1,
+			"drive": gin.H{
+				"used":  util.FSize(float64(used)),
+				"total": util.FSize(float64(total)),
+			},
+			"respond_time": fmt.Sprintf("%dms", time.Since(start).Milliseconds()),
+		})
+	})
 }
